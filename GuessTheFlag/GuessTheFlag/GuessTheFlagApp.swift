8b071ab9bc33367313e491c318335f06bf02c36b
fpizelli <168946758+fpizelli@users.noreply.github.com>
2024-09-21 15:04:28 -0300

    🎉 initial commit!

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Felipe Pizelli on 22/08/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
