86524e1b37756ac32c4bb42a39681e18c50bda46
fpizelli <168946758+fpizelli@users.noreply.github.com>
2024-09-21 15:05:58 -0300

    🎉 initial commit!

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Felipe Pizelli on 08/09/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
