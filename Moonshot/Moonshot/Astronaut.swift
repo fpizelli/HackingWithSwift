8b450ce007f0f81372f04793ce1a6085379532e5
fpizelli <168946758+fpizelli@users.noreply.github.com>
2024-09-21 15:27:36 -0300

    🎉 initial commit!

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astrounaut.swift
+//  Moonshot
+//
+//  Created by Felipe Pizelli on 11/09/24.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Felipe Pizelli on 10/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
