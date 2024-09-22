fd105b470cc75e498ee7303c1d5941d3d6fdfb57
fpizelli <168946758+fpizelli@users.noreply.github.com>
2024-09-22 05:52:21 -0300

    🎉 initial commit!

---
+++ CupcakeCorner/CupcakeCorner/AdressView.swift
@@ -0,0 +1,36 @@
+//
+//  AdressView.swift
+//  CupcakeCorner
+//
+//  Created by Felipe Pizelli on 22/09/24.
+//
+
+import SwiftUI
+
+struct AdressView: View {
+    @Bindable var order: Order
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street Adress", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+            
+            Section {
+                NavigationLink("Check out") {
+                    CheckoutView(order: order)
+                }
+            }
+            .disabled(order.hasValidAddress == false)
+        }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    AdressView(order: Order())
+}
--- CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -1,14 +1,14 @@
 //
-//  AnimationsApp.swift
-//  Animations
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
 //
-//  Created by Felipe Pizelli on 09/09/24.
+//  Created by Felipe Pizelli on 21/09/24.
 //
 
 import SwiftUI
 
 @main
-struct AnimationsApp: App {
+struct CupcakeCornerApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
