3b1a7222f7a23a9cc94734d1be066dbe870d8952
fpizelli <168946758+fpizelli@users.noreply.github.com>
2024-10-01 13:37:44 -0300

    🎉 initial commit!

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Felipe Pizelli on 28/09/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
